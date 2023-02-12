c23170743271aaa0e4525b2aee8533edcdd8af1c
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2023-02-12 18:19:31 +0900

    Initial commit

---
+++ src/main/java/memo/memospring/MemoSpringApplication.java
@@ -0,0 +1,13 @@
+package memo.memospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/memo/memospring/MemoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package memo.memospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
